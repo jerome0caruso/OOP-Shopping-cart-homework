e561ad0a2685dfb0bcb7f1a1c79ae7d44d17c402
Jerome Caruso <jerome@myjavascriptprojects.com>
2021-09-24 10:10:29 -0500

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -16,3 +16,20 @@ string_1 = StringStuff()
 
 string_1.get_String()
 
+# ****another way****
+
+class StringStuff():
+
+    def __init__(self, u_input = ""):
+        self.u_input = u_input
+
+    def get_String(self):
+        self.u_input = input("Please enter a sentance!")
+        self.print_String()
+
+    def print_String(self):
+        print(self.u_input.upper())
+
+string_1 = StringStuff()
+
+string_1.get_String()
